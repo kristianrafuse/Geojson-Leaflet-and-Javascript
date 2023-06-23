bd87c8e9b993ac5a36ab51f6119419fc38770610
kristianrafuse <kristianrafuse@gmail.com>
2023-06-23 10:39:44 -0300

    update

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,65 +1,56 @@
-// Store our API endpoint as queryUrl.
-let queryUrl = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2021-01-01&endtime=2021-01-02&maxlongitude=-69.52148437&minlongitude=-123.83789062&maxlatitude=48.74894534&minlatitude=25.16517337";
-
-// Perform a GET request to the query URL/
-d3.json(queryUrl).then(function (data) {
-  // Once we get a response, send the data.features object to the createFeatures function.
-  createFeatures(data.features);
-});
-
-function createFeatures(earthquakeData) {
-
-  // Define a function that we want to run once for each feature in the features array.
-  // Give each feature a popup that describes the place and time of the earthquake.
-  function onEachFeature(feature, layer) {
-    layer.bindPopup(`<h3>${feature.properties.place}</h3><hr><p>${new Date(feature.properties.time)}</p>`);
+var map = L.map('map', {
+    minZoom: 1,
+    maxZoom: 10
+  }).setView([35.75, -90.89], 4);
+  
+  // Add a tile layer to the map (you can choose a different tile provider if needed)
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+    maxZoom: 18,
+  }).addTo(map);
+  
+  $.ajax({
+    url: 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson',
+    method: 'GET',
+    dataType: 'json',
+    success: function(data) {
+      plotEarthquakes(data);
+    },
+  });
+  
+  function plotEarthquakes(data) {
+    data.features.forEach(function(feature) {
+      var lat = feature.geometry.coordinates[1];
+      var lon = feature.geometry.coordinates[0];
+      var magnitude = feature.properties.mag;
+      var depth = feature.geometry.coordinates[2];
+  
+      var marker = L.circleMarker([lat, lon], {
+        radius: magnitude * 2,
+        fillColor: getColor(depth),
+        color: '#000',
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+      }).addTo(map);
+  
+      marker.bindPopup(
+        '<strong>Place:</strong> ' + feature.properties.place + '<br>' +
+        '<strong>Magnitude:</strong> ' + magnitude + '<br>' +
+        '<strong>Depth:</strong> ' + depth + ' km'
+      );
+    });
   }
-  // Create a GeoJSON layer that contains the features array on the earthquakeData object.
-  // Run the onEachFeature function once for each piece of data in the array.
-  let earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
-  });
-
-  // Send our earthquakes layer to the createMap function/
-  createMap(earthquakes);
-}
-
-function createMap(earthquakes) {
-
-  // Create the base layers.
-  let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  })
-
-  let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-  });
-
-  // Create a baseMaps object.
-  let baseMaps = {
-    "Street Map": street,
-    "Topographic Map": topo
-  };
-
-  // Create an overlay object to hold our overlay.
-  let overlayMaps = {
-    Earthquakes: earthquakes
-  };
-
-  // Create our map, giving it the streetmap and earthquakes layers to display on load.
-  let myMap = L.map("map", {
-    center: [
-      37.09, -95.71
-    ],
-    zoom: 5,
-    layers: [street, earthquakes]
-  });
-
-  // Create a layer control.
-  // Pass it our baseMaps and overlayMaps.
-  // Add the layer control to the map.
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(myMap);
-
-}
+  
+  function getColor(depth) {
+    if (depth < 10) {
+      return '#FF0000'; // Red
+    } else if (depth < 30) {
+      return '#FFA500'; // Orange
+    } else if (depth < 50) {
+      return '#FFFF00'; // Yellow
+    } else {
+      return '#008000'; // Green
+    }
+  }
+  
